842061b503dc352b36615bedc6f69df2b3747321
Zach <zkaarvik@gmail.com>
2014-07-27 22:16:48 -0400

    API key not hardcoded. Now saved

--- popup.js
+++ popup.js
@@ -1,16 +1,36 @@
-var tekSavvyApi = {
+var tekSavvy = {
 
   sUsageURL: "https://api.teksavvy.com/web/Usage/UsageSummaryRecords?$filter=IsCurrent%20eq%20true",
 
-  requestUsage: function() {
+  initialization: function() {
+    //Set function to save API key when "set" button is pressed
+    document.getElementById("setApiKeyButton").addEventListener("click", this.onSetApiKey);
+
+    //Retreive API key - if saved then request usage
+    chrome.storage.sync.get('apiKey', function(data) {
+        if(data.apiKey) {
+            tekSavvy.requestUsage(data.apiKey);
+            var oApiKeyDiv = document.getElementById("apiKey");
+            oApiKeyDiv.value = data.apiKey;
+        }
+    });
+  },
+
+  requestUsage: function(sApiKey) {
     var req = new XMLHttpRequest();
     req.open("GET", this.sUsageURL, true);
-    req.setRequestHeader("TekSavvy-APIKey", "408F3085E995B8E30EAE2C1F3E4B6F37");
+    req.setRequestHeader("TekSavvy-APIKey", sApiKey);
     req.onload = this.processUsage.bind(this);
     req.send(null);
   },
 
   processUsage: function (e) {
+    //API Key bad - display error 
+    if(!e.target.response) {
+        document.getElementById("currentMonthAmount").innerHTML = "Error retreiving data: Check API Key";
+        return;
+    }
+
     var oUsage = JSON.parse(e.target.response);
 
     var iPeakDownload = oUsage.value[0].OnPeakDownload;
@@ -19,13 +39,23 @@ var tekSavvyApi = {
     var iOffPeakDownload = oUsage.value[0].OffPeakDownload;
     var iOffPeakUpload = oUsage.value[0].OffPeakUpload;
 
-    var oCurrentDiv = document.getElementById("currentMonthAmount"); 
-    oCurrentDiv.innerHTML = (iPeakDownload+iPeakUpload).toString();
+    document.getElementById("currentMonthAmount").innerHTML = (iPeakDownload+iPeakUpload).toString();
 
   },
+
+  onSetApiKey: function(e) {
+    var oApiKeyDiv = document.getElementById("apiKey");
+    var sApiKey = oApiKeyDiv.value;
+
+    chrome.storage.sync.set({'apiKey': sApiKey}, function() {
+        if(this.args[1].apiKey) {
+            tekSavvy.requestUsage(this.args[1].apiKey);
+        }
+    });
+  }
 
 };
 
 document.addEventListener('DOMContentLoaded', function () {
-  tekSavvyApi.requestUsage();
+    tekSavvy.initialization();
 });
